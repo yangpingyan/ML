a71ba7fac7f5a6dcbcaf410e620515a41fc9437e
yangpingyan <yangpingyan@gmail.com>
2018-09-21 18:12:11 +0800

    1

--- titanic/titanic_dm.py
+++ titanic/titanic_dm.py
@@ -1,37 +1,5 @@
 # coding: utf-8
 
-# # 蜜宝大数据风控解决方案
-#
-# ---
-#
-# ## 会议主题：
-# 1. 审核大数据风控可行性。
-# 2. 工作计划。
-#
-#
-# ## 数据挖掘工作流程
-#
-# 目前大数据风控做的第一件事是数据挖掘工作，数据挖掘的工作流程分下面七步完成：
-#
-# 1. 目标或问题定义。
-# 2. 获取数据。
-# 3. 数据分析。
-# 4. 数据清洗、特征处理。
-# 5. 机器学习训练、预测。
-# 6. 结果评估和报告。
-# 7. 总结。
-#
-#
-# ## 目标或问题定义
-#
-# 当我们面对客户提交的租赁设备订单请求时，我们有2个核心问题需要解决，一个是这个客户信用如何，是不是来欺诈的；另一个是这个客户是信用良好客户，但我们不确定这个设备的价格是否超出他所能承受的范围。因此，我们的任务目标是两个：
-# 1. 客户分类。把客户分成审核通过和审核拒绝两类。
-# 2. 确定客户信用额度。
-# 接下来的数据挖掘工作是实现客户分类的。
-
-# ## 开始数据挖掘工作
-# 先做些代码初始化
-
 import csv
 import json
 import seaborn as sns
@@ -46,116 +14,11 @@ from sklearn.preprocessing import LabelEncoder
 import warnings
 
 
-def missing_values_table(df):
-    # Total missing values
-    mis_val = df.isnull().sum()
-
-    # Percentage of missing values
-    mis_val_percent = 100 * df.isnull().sum() / len(df)
-
-    # Make a table with the results
-    mis_val_table = pd.concat([mis_val, mis_val_percent], axis=1)
-
-    # Rename the columns
-    mis_val_table_ren_columns = mis_val_table.rename(
-        columns={0: 'Missing Values', 1: '% of Total Values'})
-
-    # Sort the table by percentage of missing descending
-    mis_val_table_ren_columns = mis_val_table_ren_columns[
-        mis_val_table_ren_columns.iloc[:, 1] != 0].sort_values(
-        '% of Total Values', ascending=False).round(1)
-
-    # Print some summary information
-    print("Your selected dataframe has " + str(df.shape[1]) + " columns.\n"
-                                                              "There are " + str(mis_val_table_ren_columns.shape[0]) +
-          " columns that have missing values.")
-
-    # Return the dataframe with missing information
-    return mis_val_table_ren_columns
-
-
-# 特征分析
-def feature_analyse(df, col, bins=10):
-    if df[col].dtype != 'O':
-        col_band = col + '_band'
-        df[col_band] = pd.cut(df[col], bins).astype(str)
-        col_ana = col_band
-    else:
-        col_ana = col
-
-    print(df[col_ana].describe())
-    print("-------------------------------------------")
-    pass_df = pd.DataFrame({'pass': df[df['check_result'] == 1][col_ana].value_counts()})
-    reject_df = pd.DataFrame({'reject': df[df['check_result'] == 0][col_ana].value_counts()})
-    all_df = pd.DataFrame({'all': df[col_ana].value_counts()})
-    analyse_df = all_df.merge(pass_df, how='outer', left_index=True, right_index=True)
-    analyse_df = analyse_df.merge(reject_df, how='outer', left_index=True, right_index=True)
-    analyse_df['pass_rate'] = analyse_df['pass'] / analyse_df['all']
-    analyse_df.sort_values(by='pass_rate', inplace=True, ascending=False)
-    analyse_df.fillna(value=0, inplace=True)
-    print(analyse_df)
-    plt.bar(analyse_df.index, analyse_df['pass_rate'])
-    plt.ylabel('Pass Rate')
-    plt.show()
-
-
-# KDE plot
-def feature_kdeplot(df, feature):
-    sns.kdeplot(df.loc[df['check_result'] == 0, feature], label='reject')
-    sns.kdeplot(df.loc[df['check_result'] == 1, feature], label='pass')
-
-    plt.xlabel(feature)
-    plt.ylabel('Density')
-    plt.title('Distribution of ' + feature)
-
-
-def plot_feature_importances(df):
-    """
-    Plot importances returned by a model. This can work with any measure of
-    feature importance provided that higher importance is better.
-
-    Args:
-        df (dataframe): feature importances. Must have the features in a column
-        called `features` and the importances in a column called `importance
-
-    Returns:
-        shows a plot of the 15 most importance features
-
-        df (dataframe): feature importances sorted by importance (highest to lowest)
-        with a column for normalized importance
-        """
-
-    # Sort features according to importance
-    df = df.sort_values('importance', ascending=False).reset_index()
-
-    # Normalize the feature importances to add up to one
-    df['importance_normalized'] = df['importance'] / df['importance'].sum()
-
-    # Make a horizontal bar chart of feature importances
-    plt.figure(figsize=(10, 6))
-    ax = plt.subplot()
-
-    # Need to reverse the index to plot most important on top
-    ax.barh(list(reversed(list(df.index[:15]))),
-            df['importance_normalized'].head(15),
-            align='center', edgecolor='k')
-
-    # Set the yticks and labels
-    ax.set_yticks(list(reversed(list(df.index[:15]))))
-    ax.set_yticklabels(df['feature'].head(15))
-
-    # Plot labeling
-    plt.xlabel('Normalized Importance');
-    plt.title('Feature Importances')
-    plt.show()
-
-    return df
-
 
 warnings.filterwarnings('ignore')
 # to make output display better
-pd.set_option('display.max_columns', 10)
-pd.set_option('display.max_rows', 1000)
+pd.set_option('display.max_columns', 20)
+pd.set_option('display.max_rows', 10)
 pd.set_option('display.width', 2000)
 plt.rcParams['axes.labelsize'] = 14
 plt.rcParams['xtick.labelsize'] = 12
@@ -168,16 +31,15 @@ csv.field_size_limit(100000000)
 # ## 获取数据
 # 数据已经从数据库中导出成csv文件，直接读取即可。后面数据的读取更改为从备份数据库直接读取，不仅可以保证数据的完整，还可以避免重名字段处理的麻烦。
 PROJECT_ROOT_DIR = os.getcwd()
-DATA_ID = "学校数据.csv"
-DATASETS_PATH = os.path.join(PROJECT_ROOT_DIR, "datasets", DATA_ID)
+DATA_ID = "train.csv"
+DATASETS_PATH = os.path.join(PROJECT_ROOT_DIR, "titanic\datasets", DATA_ID)
 df_alldata = pd.read_csv(DATASETS_PATH, encoding='utf-8', engine='python')
 print("初始数据量: {}".format(df_alldata.shape))
 
 # ## 数据简单计量分析
-# 首5行数据
-df_alldata.head()
-# 最后5行数据
-df_alldata.tail()
+# 查看头尾数据
+df_alldata
+
 # 所有特征值
 df_alldata.columns.values
 
@@ -185,26 +47,11 @@ df_alldata.columns.values
 # f_classif
 # mutual_info_classif
 
-
 # 我们并不需要所有的特征值，筛选出一些可能有用的特质值
 df = df_alldata.dropna(axis=1, how='all')
-features = ['create_time', 'goods_name', 'cost', 'discount', 'pay_num', 'added_service', 'first_pay', 'channel',
-            'pay_type', 'merchant_id', 'goods_type', 'lease_term', 'daily_rent', 'accident_insurance', 'type',
-            'freeze_money', 'ip', 'releted', 'order_type', 'source', 'disposable_payment_discount',
-            'disposable_payment_enabled', 'lease_num', 'original_daily_rent', 'deposit', 'zmxy_score', 'card_id',
-            'contact', 'phone', 'provice', 'city', 'regoin', 'receive_address', 'emergency_contact_name', 'phone_book',
-            'emergency_contact_phone', 'emergency_contact_relation', 'type.1', 'detail_json', 'price', 'old_level']
-result = ['state', 'cancel_reason', 'check_result', 'check_remark', 'result']
-df = df[result + features]
 print("筛选出所有可能有用特征后的数据量: {}".format(df.shape))
-# 数据的起止时间段
-print("数据起止时间段：{} -- {}".format(df['create_time'].iloc[0], df['create_time'].iloc[-1]))
-# 订单审核结果分类
-df['check_result'].value_counts()
-# 订单状态
-df['state'].value_counts()
-# 查看非空值个数， 数据类型
-df.info()
+
+# 查看数据类型
 df.dtypes.value_counts()
 # 缺失值比率
 missing_values_table(df)
@@ -214,123 +61,16 @@ df.select_dtypes('object').apply(pd.Series.nunique, axis=0)
 df.describe()
 # 类别描述
 df.describe(include='O')
+
 # 开始清理数据
-print("初始数据量: {}".format(df.shape))
-# 丢弃身份证号为空的数据
-df.dropna(subset=['card_id'], inplace=True)
-print("去除无身份证号后的数据量: {}".format(df.shape))
-# 取有审核结果的数据
-df = df[df['check_result'].str.contains('SUCCESS|FAILURE', na=False)]
-print("去除未经机审用户后的数据量: {}".format(df.shape))
-# 去除测试数据和内部员工数据
-df = df[df['cancel_reason'].str.contains('测试|内部员工') != True]
-df = df[df['check_remark'].str.contains('测试|内部员工') != True]
-print("去除测试数据和内部员工后的数据量: {}".format(df.shape))
-# 去掉用户自己取消的数据   问题：即使用户取消了，仍然会有审核？？
-df = df[df['state'].str.match('user_canceled') != True]
-print("去除用户自己取消后的数据量: {}".format(df.shape))
-# 去除身份证重复的订单：
-df.drop_duplicates(subset=['card_id'], keep='last', inplace=True)
-print("去除身份证重复的订单后的数据量: {}".format(df.shape))
-
-# 所有字符串变成大写字母
-objs_df = pd.DataFrame({"isobj": pd.Series(df.dtypes == 'object')})
-df[objs_df[objs_df['isobj'] == True].index.values] = df[objs_df[objs_df['isobj'] == True].index.values].applymap(
-    lambda x: x.upper() if isinstance(x, str) else x)
-
-# 隐藏身份证信息
-df['card_id'] = df['card_id'].map(lambda x: x.replace(x[10:16], '******') if isinstance(x, str) else x)
-
-# 处理running_overdue 和 return_overdue 的逾期 的 check_result
-df.loc[df['state'].str.contains('overdue') == True, 'check_result'] = 'FAILURE'
-df['check_result'] = df['check_result'].apply(lambda x: 1 if 'SUCCESS' in x else 0)
-
-# 有phone_book的赋值成1， 空的赋值成0
-df['phone_book'][df['phone_book'].notnull()] = 1
-df['phone_book'][df['phone_book'].isnull()] = 0
-# 根据create_time 按时间段分类
-df['create_hour'] = df['create_time'].map(lambda x: int(x[-8:-6]))
-df['create_time_cat'] = df['create_hour'].map(lambda x: 0 if 0 < x < 7 else 1)
-# 同盾白骑士审核结果统一
-df['result'] = df['result'].map(lambda x: x.upper() if isinstance(x, str) else 'NODATA')
-df['result'][df['result'].str.match('ACCEPT')] = 'PASS'
-# 有emergency_contact_phone的赋值成1， 空的赋值成0
-df['emergency_contact_phone'][df['emergency_contact_phone'].notnull()] = 1
-df['emergency_contact_phone'][df['emergency_contact_phone'].isnull()] = 0
-
-# 处理芝麻信用分 '>600' 更改成600
-row = 0
-zmf = [0] * len(df)
-xbf = [0] * len(df)
-for x in df['zmxy_score']:
-    # print(x, row)
-    if isinstance(x, str):
-        if '/' in x:
-            score = x.split('/')
-            xbf[row] = 0 if score[0] == '' else (float(score[0]))
-            zmf[row] = 0 if score[1] == '' else (float(score[1]))
-            # print(score, row)
-        elif '>' in x:
-            zmf[row] = 600
-        else:
-            score = float(x)
-            if score <= 200:
-                xbf[row] = (score)
-            else:
-                zmf[row] = (score)
-
-    row += 1
-
-df['zmf_score'] = zmf
-df['xbf_score'] = xbf
-df['zmf_score'][df['zmf_score'] == 0] = 600
-df['xbf_score'][df['xbf_score'] == 0] = 87.6
-
-# 根据身份证号增加性别和年龄 年龄的计算需根据订单创建日期计算
-df['age'] = df['card_id'].map(lambda x: 2018 - int(x[6:10]))
-df['sex'] = df['card_id'].map(lambda x: int(x[-2]) % 2)
-
-features_cat = ['check_result', 'result', 'pay_num', 'channel', 'goods_type', 'lease_term', 'type', 'order_type',
-                'source', 'phone_book', 'emergency_contact_phone', 'old_level', 'create_hour', 'sex', ]
-features_number = ['cost', 'daily_rent', 'price', 'age', 'zmf_score', 'xbf_score', ]
-
-for col in df[features_cat + features_number].columns.values:
-    if df[col].dtype == 'O':
-        df[col].fillna(value='NODATA', inplace=True)
-df.fillna(value=0, inplace=True)
-
-plt.hist(df['check_result'])
-feature_analyse(df, 'result')
-feature_analyse(df, 'pay_num')
-feature_analyse(df, 'channel')
-
-feature = 'zmf_score'
+features = ['Survived', 'Pclass', 'Name', 'Sex', 'Age', 'SibSp', 'Parch', 'Ticket', 'Fare', 'Cabin',
+            'Embarked']
+label = 'Survived'
+feature = 'Pclass'
+plt.figure()
 plt.hist(df[feature])
-feature_analyse(df, feature)
-feature_kdeplot(df, feature)
-
-# 芝麻分分类
-bins = pd.IntervalIndex.from_tuples([(0, 600), (600, 700), (700, 800), (800, 1000)])
-df['zmf_score_band'] = pd.cut(df['zmf_score'], bins, labels=False)
-df[['zmf_score_band', 'check_result']].groupby(['zmf_score_band'], as_index=False).mean().sort_values(by='check_result',
-                                                                                                      ascending=False)
-
-# 小白分分类
-bins = pd.IntervalIndex.from_tuples([(0, 80), (80, 90), (90, 100), (100, 200)])
-df['xbf_score_band'] = pd.cut(df['xbf_score'], bins, labels=False)
-df[['xbf_score_band', 'check_result']].groupby(['xbf_score_band'], as_index=False).mean().sort_values(by='check_result',
-                                                                                                      ascending=False)
-
-# 年龄分类
-bins = pd.IntervalIndex.from_tuples([(0, 18), (18, 24), (24, 30), (30, 40), (40, 100)])
-df['age_band'] = pd.cut(df['age'], bins, labels=False)
-df[['age_band', 'check_result']].groupby(['age_band'], as_index=False).mean().sort_values(by='check_result',
-                                                                                          ascending=False)
-
-# 下单时间分类
-df['create_hour_band'] = pd.cut(df['create_hour'], 5, labels=False)
-df[['create_hour_band', 'check_result']].groupby(['create_hour_band'], as_index=False).mean().sort_values(
-    by='check_result', ascending=False)
+feature_analyse(df, feature, label)
+feature_kdeplot(df, feature, label)
 
 features = ['check_result', 'result', 'pay_num', 'channel', 'goods_type', 'type', 'order_type',
             'source', 'phone_book', 'old_level', 'sex', 'create_hour', 'age_band', 'zmf_score_band',
---
+++ tools_ml.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python 
+# coding: utf-8
+# @Time : 2018/9/21 17:58 
+# @Author : yangpingyan@gmail.com
+
+import csv
+import json
+import seaborn as sns
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+import time
+import os
+
+
+def missing_values_table(df):
+    # Total missing values
+    mis_val = df.isnull().sum()
+
+    # Percentage of missing values
+    mis_val_percent = 100 * df.isnull().sum() / len(df)
+
+    # Make a table with the results
+    mis_val_table = pd.concat([mis_val, mis_val_percent], axis=1)
+
+    # Rename the columns
+    mis_val_table_ren_columns = mis_val_table.rename(
+        columns={0: 'Missing Values', 1: '% of Total Values'})
+
+    # Sort the table by percentage of missing descending
+    mis_val_table_ren_columns = mis_val_table_ren_columns[
+        mis_val_table_ren_columns.iloc[:, 1] != 0].sort_values(
+        '% of Total Values', ascending=False).round(1)
+
+    # Print some summary information
+    print("Your selected dataframe has " + str(df.shape[1]) + " columns.\n"
+                                                              "There are " + str(mis_val_table_ren_columns.shape[0]) +
+          " columns that have missing values.")
+
+    # Return the dataframe with missing information
+    return mis_val_table_ren_columns
+
+
+# 特征分析
+def feature_analyse(df, col, label='check_result', bins=10):
+    if df[col].dtype != 'O':
+        col_band = col + '_band'
+        df[col_band] = pd.cut(df[col], bins).astype(str)
+        col_ana = col_band
+    else:
+        col_ana = col
+
+    print(df[col_ana].describe())
+    print("-------------------------------------------")
+    pass_df = pd.DataFrame({'positive': df[df[label] == 1][col_ana].value_counts()})
+    reject_df = pd.DataFrame({'negative': df[df[label] == 0][col_ana].value_counts()})
+    all_df = pd.DataFrame({'all': df[col_ana].value_counts()})
+    analyse_df = all_df.merge(pass_df, how='outer', left_index=True, right_index=True)
+    analyse_df = analyse_df.merge(reject_df, how='outer', left_index=True, right_index=True)
+    analyse_df['positive_rate'] = analyse_df['positive'] / analyse_df['all']
+    analyse_df.sort_values(by='positive_rate', inplace=True, ascending=False)
+    analyse_df.fillna(value=0, inplace=True)
+    print(analyse_df)
+    plt.figure()
+    plt.bar(analyse_df.index, analyse_df['pass_rate'])
+    plt.ylabel('Positive Rate')
+    plt.title('Positive Rate of ' + feature)
+
+
+# KDE plot
+def feature_kdeplot(df, feature, label='check_result'):
+    plt.figure()
+    sns.kdeplot(df.loc[df[label] == 0, feature], label='0')
+    sns.kdeplot(df.loc[df[label] == 1, feature], label='1')
+    plt.xlabel(feature)
+    plt.ylabel('Density')
+    plt.title('Distribution of ' + feature)
+
+def plot_feature_importances(df):
+    """
+    Plot importances returned by a model. This can work with any measure of
+    feature importance provided that higher importance is better.
+
+    Args:
+        df (dataframe): feature importances. Must have the features in a column
+        called `features` and the importances in a column called `importance
+
+    Returns:
+        shows a plot of the 15 most importance features
+
+        df (dataframe): feature importances sorted by importance (highest to lowest)
+        with a column for normalized importance
+        """
+
+    # Sort features according to importance
+    df = df.sort_values('importance', ascending=False).reset_index()
+
+    # Normalize the feature importances to add up to one
+    df['importance_normalized'] = df['importance'] / df['importance'].sum()
+
+    # Make a horizontal bar chart of feature importances
+    plt.figure(figsize=(10, 6))
+    ax = plt.subplot()
+
+    # Need to reverse the index to plot most important on top
+    ax.barh(list(reversed(list(df.index[:15]))),
+            df['importance_normalized'].head(15),
+            align='center', edgecolor='k')
+
+    # Set the yticks and labels
+    ax.set_yticks(list(reversed(list(df.index[:15]))))
+    ax.set_yticklabels(df['feature'].head(15))
+
+    # Plot labeling
+    plt.xlabel('Normalized Importance');
+    plt.title('Feature Importances')
+    plt.show()
+
+    return df
