511d0dd560ca785c00b589847b8baaff4abc0294
yangpingyan <yangpingyan@gmail.com>
2018-09-27 02:10:02 +0800

    1

--- tools_ml.py
+++ tools_ml.py
@@ -15,6 +15,130 @@ import os
 from sklearn.metrics import precision_recall_curve, precision_score, recall_score, f1_score
 from sklearn.metrics import roc_curve, roc_auc_score, confusion_matrix, accuracy_score
 
+
+def agg_numeric(df, group_var, df_name):
+    """Aggregates the numeric values in a dataframe. This can
+    be used to create features for each instance of the grouping variable.
+
+    Parameters
+    --------
+        df (dataframe):
+            the dataframe to calculate the statistics on
+        group_var (string):
+            the variable by which to group df
+        df_name (string):
+            the variable used to rename the columns
+
+    Return
+    --------
+        agg (dataframe):
+            a dataframe with the statistics aggregated for
+            all numeric columns. Each instance of the grouping variable will have
+            the statistics (mean, min, max, sum; currently supported) calculated.
+            The columns are also renamed to keep track of features created.
+
+    """
+    # Remove id variables other than grouping variable
+    for col in df:
+        if col != group_var and 'SK_ID' in col:
+            df = df.drop(columns=col)
+
+    group_ids = df[group_var]
+    numeric_df = df.select_dtypes('number')
+    numeric_df[group_var] = group_ids
+
+    # Group by the specified variable and calculate the statistics
+    agg = numeric_df.groupby(group_var).agg(['count', 'mean', 'max', 'min', 'sum']).reset_index()
+
+    # Need to create new column names
+    columns = [group_var]
+
+    # Iterate through the variables names
+    for var in agg.columns.levels[0]:
+        # Skip the grouping variable
+        if var != group_var:
+            # Iterate through the stat names
+            for stat in agg.columns.levels[1][:-1]:
+                # Make a new column name for the variable and stat
+                columns.append('%s_%s_%s' % (df_name, var, stat))
+
+    agg.columns = columns
+    return agg
+
+
+bureau_agg_new = agg_numeric(bureau.drop(columns=['SK_ID_BUREAU']), group_var='SK_ID_CURR', df_name='bureau')
+bureau_agg_new.head()
+
+
+# Function to calculate correlations with the target for a dataframe
+def target_corrs(df):
+    # List of correlations
+    corrs = []
+
+    # Iterate through the columns
+    for col in df.columns:
+        print(col)
+        # Skip the target column
+        if col != 'TARGET':
+            # Calculate correlation with the target
+            corr = df['TARGET'].corr(df[col])
+
+            # Append the list as a tuple
+            corrs.append((col, corr))
+
+    # Sort by absolute magnitude of correlations
+    corrs = sorted(corrs, key=lambda x: abs(x[1]), reverse=True)
+
+    return corrs
+
+
+def count_categorical(df, group_var, df_name):
+    """Computes counts and normalized counts for each observation
+    of `group_var` of each unique category in every categorical variable
+
+    Parameters
+    --------
+    df : dataframe
+        The dataframe to calculate the value counts for.
+
+    group_var : string
+        The variable by which to group the dataframe. For each unique
+        value of this variable, the final dataframe will have one row
+
+    df_name : string
+        Variable added to the front of column names to keep track of columns
+
+
+    Return
+    --------
+    categorical : dataframe
+        A dataframe with counts and normalized counts of each unique category in every categorical variable
+        with one row for every unique value of the `group_var`.
+
+    """
+
+    # Select the categorical columns
+    categorical = pd.get_dummies(df.select_dtypes('object'))
+
+    # Make sure to put the identifying id on the column
+    categorical[group_var] = df[group_var]
+
+    # Groupby the group var and calculate the sum and mean
+    categorical = categorical.groupby(group_var).agg(['sum', 'mean'])
+
+    column_names = []
+
+    # Iterate through the columns in level 0
+    for var in categorical.columns.levels[0]:
+        # Iterate through the stats in level 1
+        for stat in ['count', 'count_norm']:
+            # Make a new column name
+            column_names.append('%s_%s_%s' % (df_name, var, stat))
+
+    categorical.columns = column_names
+
+    return categorical
+
 def pairs_plot(plot_data, target='TARGET'):
     # Create the pairgrid object
     grid = sns.PairGrid(data=plot_data, size=3, diag_sharey=False,
