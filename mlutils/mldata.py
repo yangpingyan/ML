375029bb301d6d7db10e8e55e22c7c1e5ca87a97
yangpingyan <yangpingyan@gmail.com>
2018-09-28 17:27:56 +0800

    1

--- mlutils/mldata.py
+++ mlutils/mldata.py
@@ -1,2 +1,26 @@
-# flake8: noqa
-from .mldata import load_mibao_data
+#!/usr/bin/env python
+# coding: utf-8
+# @Time : 2018/9/28 16:42
+# @Author : yangpingyan@gmail.com
+
+import pandas as pd
+from sqlalchemy import create_engine
+
+def load_data_mibao():
+    df = load_data_mibao_sql()
+    return df
+
+def load_data_mibao_sql():
+    # 初始化数据库连接，使用pymysql模块
+    engine = create_engine('mysql+pymysql://root:qawsedrf@localhost:3306/mibao')
+
+    # 查询语句，选出employee表中的所有数据
+    sql = '''
+          select * from `admin`;
+          '''
+
+    # read_sql_query的两个参数: sql语句， 数据库连接
+    df = pd.read_sql_query(sql, engine)
+
+    return df
+
