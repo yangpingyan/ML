f5c9dcdbd5df4c44d154433683a0e209564ea3ec
yangpingyan <yangpingyan@gmail.com>
2018-09-28 17:19:27 +0800

    1

--- load_data.py
+++ load_data.py
@@ -9,11 +9,11 @@ from sqlalchemy import create_engine
 
 # 初始化数据库连接，使用pymysql模块
 # MySQL的用户：root, 密码:147369, 端口：3306,数据库：mydb
-engine = create_engine('mysql+pymysql://root:147369@localhost:3306/mydb')
+engine = create_engine('mysql+pymysql://root:qawsedrf@localhost:3306/mibao')
 
 # 查询语句，选出employee表中的所有数据
 sql = '''
-      select * from employee;
+      select * from `order`;
       '''
 
 # read_sql_query的两个参数: sql语句， 数据库连接
@@ -23,14 +23,7 @@ df = pd.read_sql_query(sql, engine)
 print(df)
 
 # 新建pandas中的DataFrame, 只有id,num两列
-df = pd.DataFrame({'id':[1,2,3,4],'num':[12,34,56,89]})
+# df = pd.DataFrame({'id':[1,2,3,4],'num':[12,34,56,89]})
 
 # 将新建的DataFrame储存为MySQL中的数据表，不储存index列
-df.to_sql('mydf', engine, index= False)
-
-print('Read from and write to Mysql table successfully!')
-
-作者：但盼风雨来_jc
-链接：https://www.jianshu.com/p/238a13995b2b
-來源：简书
-简书著作权归作者所有，任何形式的转载都请联系作者获得授权并注明出处。
+# df.to_sql('mydf', engine, index= False)
---
+++ mlutils/mldata.py
@@ -0,0 +1,2 @@
+# flake8: noqa
+from .mldata import load_mibao_data
