2fd01b5c6d72d85671a44f62d6c8822b17277d30
yangpingyan <yangpingyan@gmail.com>
2018-10-08 17:12:59 +0800

    1

--- mibao/mibao_ml.py
+++ mibao/mibao_ml.py
@@ -22,6 +22,11 @@ from sklearn.metrics import precision_recall_curve, precision_score, recall_scor
 from sklearn.metrics import roc_curve, roc_auc_score
 from sklearn.model_selection import KFold
 from xgboost import XGBClassifier
+import lightgbm as lgb
+from mlutils import *
+
+N_FOLDS = 5
+MAX_EVALS = 5
 
 # to make output display better
 pd.set_option('display.max_columns', 50)
@@ -31,37 +36,57 @@ plt.rcParams['axes.labelsize'] = 14
 plt.rcParams['xtick.labelsize'] = 12
 plt.rcParams['ytick.labelsize'] = 12
 # read large csv file
-PROJECT_ROOT_DIR = os.getcwd()
-DATA_ID = "mibaodata_ml.csv"
-DATASETS_PATH = os.path.join(PROJECT_ROOT_DIR, "datasets", DATA_ID)
-# Get Data
-df = pd.read_csv(DATASETS_PATH, encoding='utf-8', engine='python')
-print("ML初始数据量: {}".format(df.shape))
 
-x = df.drop(['check_result'], axis=1)
-y = df['check_result']
+# ## 获取数据
+PROJECT_ID = 'mibao'
+if os.getcwd().find(PROJECT_ID) == -1:
+    os.chdir(PROJECT_ID)
+datasets_path = os.getcwd() + '\\datasets\\'
+alldata_df = pd.read_csv("{}mibaodata_ml.csv".format(datasets_path), encoding='utf-8', engine='python')
+print("初始数据量: {}".format(alldata_df.shape))
+
+df = alldata_df.copy()
+x = df.drop(['TARGET'], axis=1)
+y = df['TARGET']
 ## Splitting the dataset into the Training set and Test set
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
-
-score_df = pd.DataFrame(index=['accuracy', 'precision', 'recall', 'f1', 'runtime', 'confusion_matrix'])
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1, random_state=88)
+score_df = pd.DataFrame(index=['accuracy', 'precision', 'recall', 'f1', 'confusion_matrix'])
 
 rnd_clf = RandomForestClassifier(random_state=0)
-starttime = time.clock()
 rnd_clf.fit(x_train, y_train)
 y_pred = rnd_clf.predict(x_test)
-add_score(score_df, rnd_clf.__class__.__name__, time.clock() - starttime, y_pred, y_test)
-y_train_pred = rnd_clf.predict(x_train)
-add_score(score_df, rnd_clf.__class__.__name__ + '_Train', time.clock() - starttime, y_train_pred, y_train)
+add_score(score_df, rnd_clf.__class__.__name__, y_pred, y_test)
 
 xgb_clf = XGBClassifier(random_state=0)
-starttime = time.clock()
 xgb_clf.fit(x_train, y_train)
 y_pred = xgb_clf.predict(x_test)
-add_score(score_df, xgb_clf.__class__.__name__, time.clock() - starttime, y_pred, y_test)
-y_train_pred = xgb_clf.predict(x_train)
-add_score(score_df, xgb_clf.__class__.__name__ + '_Train', time.clock() - starttime, y_train_pred, y_train)
+add_score(score_df, xgb_clf.__class__.__name__, y_pred, y_test)
 
 print(score_df)
+
+# Create a training and testing dataset
+train_set = lgb.Dataset(data=x_train, label=y_train)
+test_set = lgb.Dataset(data=x_test, label=y_test)
+# Get default hyperparameters
+model = lgb.LGBMClassifier()
+default_params = model.get_params()
+# Remove the number of estimators because we set this to 10000 in the cv call
+del default_params['n_estimators']
+
+# Cross validation with early stopping
+cv_results = lgb.cv(default_params, train_set, num_boost_round=10000, early_stopping_rounds=100, metrics='auc',
+                    nfold=N_FOLDS, seed=42)
+print('The maximum validation ROC AUC was: {:.5f} with a standard deviation of {:.5f}.'.format(cv_results['auc-mean'][-1], cv_results['auc-stdv'][-1]))
+print('The optimal number of boosting rounds (estimators) was {}.'.format(len(cv_results['auc-mean'])))
+
+# Optimal number of esimators found in cv
+model.n_estimators = len(cv_results['auc-mean'])
+
+# Train and make predicions with model
+model.fit(x_train, y_train)
+y_pred = model.predict(x_test)
+add_score(score_df, model.__class__.__name__, y_pred, y_test)
+
 
 '''
 调试代码
@@ -85,130 +110,6 @@ missing_values_table(df)
 
  b. 创造未被保存到数据库中的特征：年化利率,是否有2个手机号。
 '''
-
-# 丢弃身份证号为空的数据
-df.dropna(subset=['card_id'], inplace=True)
-print("去除无身份证号后的数据量: {}".format(df.shape))
-# 取有审核结果的数据
-df = df[df['check_result'].str.contains('SUCCESS|FAILURE', na=False)]
-print("去除未经机审用户后的数据量: {}".format(df.shape))
-# 去除测试数据和内部员工数据
-df = df[df['cancel_reason'].str.contains('测试|内部员工') != True]
-df = df[df['check_remark'].str.contains('测试|内部员工') != True]
-print("去除测试数据和内部员工后的数据量: {}".format(df.shape))
-# 去掉用户自己取消的数据   问题：即使用户取消了，仍然会有审核？？
-df = df[df['state'].str.match('user_canceled') != True]
-print("去除用户自己取消后的数据量: {}".format(df.shape))
-# 去除身份证重复的订单：
-df.drop_duplicates(subset=['card_id'], keep='last', inplace=True)
-print("去除身份证重复的订单后的数据量: {}".format(df.shape))
-
-# 所有字符串变成大写字母
-objs_df = pd.DataFrame({"isobj": pd.Series(df.dtypes == 'object')})
-df[objs_df[objs_df['isobj'] == True].index.values] = df[objs_df[objs_df['isobj'] == True].index.values].applymap(
-    lambda x: x.upper() if isinstance(x, str) else x)
-
-# 隐藏身份证信息
-df['card_id'] = df['card_id'].map(lambda x: x.replace(x[10:16], '******') if isinstance(x, str) else x)
-
-# 处理running_overdue 和 return_overdue 的逾期 的 check_result
-df.loc[df['state'].str.contains('overdue') == True, 'check_result'] = 'FAILURE'
-df['check_result'] = df['check_result'].apply(lambda x: 1 if 'SUCCESS' in x else 0)
-
-# 有phone_book的赋值成1， 空的赋值成0
-df['phone_book'][df['phone_book'].notnull()] = 1
-df['phone_book'][df['phone_book'].isnull()] = 0
-# 根据create_time 按时间段分类
-df['create_hour'] = df['create_time'].map(lambda x: int(x[-8:-6]))
-df['create_time_cat'] = df['create_hour'].map(lambda x: 0 if 0 < x < 7 else 1)
-# 同盾白骑士审核结果统一
-df['result'] = df['result'].map(lambda x: x.upper() if isinstance(x, str) else 'NODATA')
-df['result'][df['result'].str.match('ACCEPT')] = 'PASS'
-# 有emergency_contact_phone的赋值成1， 空的赋值成0
-df['emergency_contact_phone'][df['emergency_contact_phone'].notnull()] = 1
-df['emergency_contact_phone'][df['emergency_contact_phone'].isnull()] = 0
-
-# 处理芝麻信用分 '>600' 更改成600
-row = 0
-zmf = [0] * len(df)
-xbf = [0] * len(df)
-for x in df['zmxy_score']:
-    # print(x, row)
-    if isinstance(x, str):
-        if '/' in x:
-            score = x.split('/')
-            xbf[row] = 0 if score[0] == '' else (float(score[0]))
-            zmf[row] = 0 if score[1] == '' else (float(score[1]))
-            # print(score, row)
-        elif '>' in x:
-            zmf[row] = 600
-        else:
-            score = float(x)
-            if score <= 200:
-                xbf[row] = (score)
-            else:
-                zmf[row] = (score)
-
-    row += 1
-
-df['zmf_score'] = zmf
-df['xbf_score'] = xbf
-df['zmf_score'][df['zmf_score'] == 0] = 600
-df['xbf_score'][df['xbf_score'] == 0] = 87.6
-
-# 根据身份证号增加性别和年龄 年龄的计算需根据订单创建日期计算
-df['age'] = df['card_id'].map(lambda x: 2018 - int(x[6:10]))
-df['sex'] = df['card_id'].map(lambda x: int(x[-2]) % 2)
-
-features_cat = ['check_result', 'result', 'pay_num', 'channel', 'goods_type', 'lease_term', 'type', 'order_type',
-                'source', 'phone_book', 'emergency_contact_phone', 'old_level', 'create_hour', 'sex', ]
-features_number = ['cost', 'daily_rent', 'price', 'age', 'zmf_score', 'xbf_score', ]
-
-for col in df[features_cat + features_number].columns.values:
-    if df[col].dtype == 'O':
-        df[col].fillna(value='NODATA', inplace=True)
-df.fillna(value=0, inplace=True)
-
-plt.hist(df['check_result'])
-feature_analyse(df, 'result')
-feature_analyse(df, 'pay_num')
-feature_analyse(df, 'channel')
-
-feature = 'zmf_score'
-plt.hist(df[feature])
-feature_analyse(df, feature)
-feature_kdeplot(df, feature)
-
-# 芝麻分分类
-bins = pd.IntervalIndex.from_tuples([(0, 600), (600, 700), (700, 800), (800, 1000)])
-df['zmf_score_band'] = pd.cut(df['zmf_score'], bins, labels=False)
-df[['zmf_score_band', 'check_result']].groupby(['zmf_score_band'], as_index=False).mean().sort_values(by='check_result',
-                                                                                                      ascending=False)
-
-# 小白分分类
-bins = pd.IntervalIndex.from_tuples([(0, 80), (80, 90), (90, 100), (100, 200)])
-df['xbf_score_band'] = pd.cut(df['xbf_score'], bins, labels=False)
-df[['xbf_score_band', 'check_result']].groupby(['xbf_score_band'], as_index=False).mean().sort_values(by='check_result',
-                                                                                                      ascending=False)
-
-# 年龄分类
-bins = pd.IntervalIndex.from_tuples([(0, 18), (18, 24), (24, 30), (30, 40), (40, 100)])
-df['age_band'] = pd.cut(df['age'], bins, labels=False)
-df[['age_band', 'check_result']].groupby(['age_band'], as_index=False).mean().sort_values(by='check_result',
-                                                                                          ascending=False)
-
-# 下单时间分类
-df['create_hour_band'] = pd.cut(df['create_hour'], 5, labels=False)
-df[['create_hour_band', 'check_result']].groupby(['create_hour_band'], as_index=False).mean().sort_values(
-    by='check_result', ascending=False)
-
-features = ['check_result', 'result', 'pay_num', 'channel', 'goods_type', 'type', 'order_type',
-            'source', 'phone_book', 'old_level', 'sex', 'create_hour', 'age_band', 'zmf_score_band',
-            'xbf_score_band', ]
-df = df[features]
-# 类别特征全部转换成数字
-for feature in features:
-    df[feature] = LabelEncoder().fit_transform(df[feature])
 
 # ## 评估结果
 # accuracy： 97.5%  --- 预测正确的个数占样本总数的比率
@@ -237,25 +138,3 @@ plt.plot(fpr, tpr, linewidth=1, label="ROC")
 plt.title("ROC and PR 曲线图")
 plt.legend()
 plt.show()
-
-# ## 总结
-# 1. 机器学习能洞察复杂问题和大量数据，发现内在规律，帮助我们做好数据挖掘方面的工作。
-# 2. 很有很多的改进空间：
-#         a.现有数据特征挖掘，譬如同盾和白骑士中的内容详情，IP地址、收货地址、身份证地址、居住地址中的关联，优惠券，额外服务意外险，每日租金，手机号前三位等。与客户和订单信息相关的数据都可以拿来做特征，提高机器学习预测能力。
-#         b. 增加未被保存到数据库中的特征。
-#         c. 调整机器学习参数，使之更适合做预测分类。
-#         d. 使用更好的模型。XGBoosting、LightGBM和人工神经网络在训练和预测能力上做到更好。
-# 3. 工作计划：
-#         a. 增加特征。需数据库里保存所有订单审核前的所有信息。目前折扣金额和额外服务等数据只有再审核通过后才保存，需更改下。
-#         b. 参考同类型行业，借鉴他人经验，增加相关特征
-#         c. 增加模型预测能力，ROC分数达到0.96以上， 预测准确度达到98.5%
-#         d. 增加客户信用额度字段。如何确定客户额度方案未知。
-#
-# ## 特征处理
-# 1. 把IP地址、收货地址、身份证地址、居住地址转换成经纬度
-# 2. 把创建时间转换成月、日、周几、小时段
-
-# ## 增加特征
-# 1. 下单时的经纬度
-# 2. 下单时手机设备
-# 3.
--- mlutils/api.py
+++ mlutils/api.py
@@ -2,3 +2,4 @@
 from .mldata import load_data_mibao
 from .explore_data_utils import feature_analyse
 from .explore_data_utils import missing_values_table
+from .explore_data_utils import add_score
