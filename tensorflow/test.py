6961a1353d6d42956532d27e4059098f46f4700d
yangpingyan <yangpingyan@gmail.com>
2018-10-10 18:08:02 +0800

    1

--- tensorflow/test.py
+++ tensorflow/test.py
@@ -2,3 +2,33 @@
 # coding: utf-8
 # @Time : 2018/10/10 16:26 
 # @Author : yangpingyan@gmail.com
+
+import tensorflow as tf
+import numpy as np
+from sklearn.datasets import fetch_california_housing
+from sklearn.preprocessing import scale
+
+housing = fetch_california_housing()
+m, n = housing.data.shape
+m, n = housing.data.shape
+#np.c_按colunm来组合array
+housing_data_plus_bias = np.c_[np.ones((m, 1)), housing.data]
+scaled_housing_data_plus_bias = scale(housing_data_plus_bias)
+n_epochs = 1000
+learning_rate = 0.01
+X = tf.constant(scaled_housing_data_plus_bias, dtype=tf.float32, name="X")
+y = tf.constant(housing.target.reshape(-1, 1), dtype=tf.float32, name="y")
+theta = tf.Variable(tf.random_uniform([n + 1, 1], -1.0, 1.0), name="theta")
+y_pred = tf.matmul(X, theta, name="predictions")
+error = y_pred - y
+mse = tf.reduce_mean(tf.square(error), name="mse")
+gradients = 2/m * tf.matmul(tf.transpose(X), error)
+training_op = tf.assign(theta, theta - learning_rate * gradients)
+init = tf.global_variables_initializer()
+with tf.Session() as sess:
+    sess.run(init)
+    for epoch in range(n_epochs):
+        if epoch % 100 == 0:
+            print("Epoch", epoch, "MSE =", mse.eval())
+        sess.run(training_op)
+    best_theta = theta.eval()
