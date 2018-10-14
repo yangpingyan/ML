8a687af7fd24d0602ce4a01c49bfc3e06c78913f
yangpingyan <yangpingyan@gmail.com>
2018-10-15 00:20:03 +0800

    1

--- tensorflow/test.py
+++ tensorflow/test.py
@@ -7,28 +7,57 @@ import tensorflow as tf
 import numpy as np
 from sklearn.datasets import fetch_california_housing
 from sklearn.preprocessing import scale
+from sklearn.preprocessing import StandardScaler
+from datetime import datetime
 
 housing = fetch_california_housing()
 m, n = housing.data.shape
-m, n = housing.data.shape
-#np.c_按colunm来组合array
+print("数据集:{}行,{}列".format(m, n))
 housing_data_plus_bias = np.c_[np.ones((m, 1)), housing.data]
-scaled_housing_data_plus_bias = scale(housing_data_plus_bias)
+scaler = StandardScaler()
+scaled_housing_data = scaler.fit_transform(housing.data)
+scaled_housing_data_plus_bias = np.c_[np.ones((m, 1)), scaled_housing_data]
+from datetime import datetime
+
+now = datetime.utcnow().strftime("%Y%m%d%H%M%S")
+root_logdir = r"c://tf_logs"
+logdir = "{}/run-{}/".format(root_logdir, now)
 n_epochs = 1000
 learning_rate = 0.01
-X = tf.constant(scaled_housing_data_plus_bias, dtype=tf.float32, name="X")
-y = tf.constant(housing.target.reshape(-1, 1), dtype=tf.float32, name="y")
-theta = tf.Variable(tf.random_uniform([n + 1, 1], -1.0, 1.0), name="theta")
+X = tf.placeholder(tf.float32, shape=(None, n + 1), name="X")
+y = tf.placeholder(tf.float32, shape=(None, 1), name="y")
+theta = tf.Variable(tf.random_uniform([n + 1, 1], -1.0, 1.0, seed=42), name="theta")
 y_pred = tf.matmul(X, theta, name="predictions")
 error = y_pred - y
 mse = tf.reduce_mean(tf.square(error), name="mse")
-gradients = 2/m * tf.matmul(tf.transpose(X), error)
-training_op = tf.assign(theta, theta - learning_rate * gradients)
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
+training_op = optimizer.minimize(mse)
 init = tf.global_variables_initializer()
+mse_summary = tf.summary.scalar('MSE', mse)
+file_writer = tf.summary.FileWriter(logdir, tf.get_default_graph())
+n_epochs = 10
+batch_size = 100
+n_batches = int(np.ceil(m / batch_size))
+
+
+def fetch_batch(epoch, batch_index, batch_size):
+    np.random.seed(epoch * n_batches + batch_index)  # not shown in the book
+    indices = np.random.randint(m, size=batch_size)  # not shown
+    X_batch = scaled_housing_data_plus_bias[indices]  # not shown
+    y_batch = housing.target.reshape(-1, 1)[indices]  # not shown
+    return X_batch, y_batch
+
+
 with tf.Session() as sess:
     sess.run(init)
     for epoch in range(n_epochs):
-        if epoch % 100 == 0:
-            print("Epoch", epoch, "MSE =", mse.eval())
-        sess.run(training_op)
+        for batch_index in range(n_batches):
+            X_batch, y_batch = fetch_batch(epoch, batch_index, batch_size)
+            if batch_index % 10 == 0:
+                summary_str = mse_summary.eval(feed_dict={X: X_batch, y: y_batch})
+                step = epoch * n_batches + batch_index
+                file_writer.add_summary(summary_str, step)
+            sess.run(training_op, feed_dict={X: X_batch, y: y_batch})
     best_theta = theta.eval()
+file_writer.close()
+print(best_theta)
