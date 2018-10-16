a5e9bd4a7afe3c519f7ebef2028803179ae6ceda
yangpingyan <yangpingyan@gmail.com>
2018-10-16 21:52:48 +0800

    1

--- mibao/mibao_mergedata.py
+++ mibao/mibao_mergedata.py
@@ -52,14 +52,14 @@ state_values_newest = df['state'].unique().tolist()
 # 若state字段有新的状态产生， 抛出异常
 assert (operator.eq(state_values_newest, state_values))
 
-df = df[df['state'].isin(pending_state_values + ['user_canceled']) is not True]
+df = df[df['state'].isin(pending_state_values + ['user_canceled']) != True]
 df.insert(0, 'target', np.where(df['state'].isin(failure_state_values), 0, 1))
 
 # 去除测试数据和内部员工数据
-df = df[df['cancel_reason'].str.contains('测试') is not True]
+df = df[df['cancel_reason'].str.contains('测试') != True]
 # df = df[df['check_remark'].str.contains('测试|内部员工') != True] #order_buyout表
 # 去除命中商户白名单的订单
-df = df[df['hit_merchant_white_list'].str.contains('01') is not True]
+df = df[df['hit_merchant_white_list'].str.contains('01') != True]
 
 df.drop(['state', 'cancel_reason', 'hit_merchant_white_list'], axis=1, inplace=True, errors='ignore')
 all_data_df = df.copy()
@@ -161,7 +161,7 @@ for risk_type in df['type'].unique().tolist():
 # 未处理特征：
 df = pd.read_csv(datasets_path + "risk_white_list.csv")
 user_ids = df['user_id'].values
-all_data_df = all_data_df[all_data_df['user_id'].isin(user_ids) is not True]
+all_data_df = all_data_df[all_data_df['user_id'].isin(user_ids) != True]
 
 # ser_login_record todo
 # user_longitude_latitude todo
@@ -231,164 +231,4 @@ all_data_df.to_csv(datasets_path + "mibao.csv", index=False)
 print("mibao.csv saved")
 
 # merchant 违约率
-'''
-df.columns.values
-feature = 'have_bargain_help'
-df[feature].value_counts()
-feature_analyse(df, feature)
-df[df[feature].isnull()].sort_values(by='state').shape
 
-missing_values_table(df)
-df[feature].unique()
-'''
-df.drop(['user_id', ], axis=1, inplace=True, errors='ignore')
-
-datasets_path
-print("保存的数据量: {}".format(df.shape))
-df.to_csv(datasets_path + "mibaodata_ml.csv", index=False)
-
-# 查看各特征关联度
-plt.figure(figsize=(14, 12))
-plt.title('Pearson Correlation of Features', y=1.05, size=15)
-sns.heatmap(df.astype(float).corr(), linewidths=0.1, vmax=1.0,
-            square=True, cmap=plt.cm.RdBu, linecolor='white', annot=True)
-plt.show()
-
-'''
-调试代码
-df.sort_values(by=['device_type'], inplace=True, axis=1)
-df['device_type'].value_counts()
-df['device_type'].fillna(value='NODATA', inplace=True)
-feature_analyse(df, 'bounds_example_id')
-df.columns.values
-missing_values_table(df)
-'''
-'''
-现有数据特征挖掘（与客户和订单信息相关的数据都可以拿来做特征）：
-1. 同盾和白骑士中的内容详情
-2. IP地址、收货地址、身份证地址、居住地址中的关联
-3. 优惠券，额外服务意外险
-4. 首付金额、每日租金
-5. 手机号前三位等
-6. 下单使用的设备，通过什么客户端下单（微信、支付宝、京东、网页）
-7. 是否有推荐人， 推荐人是否通过审核
-#. 租借个数
-
- b. 创造未被保存到数据库中的特征：年化利率,是否有2个手机号。
-'''
-
-# 所有字符串变成大写字母
-objs_df = pd.DataFrame({"isobj": pd.Series(df.dtypes == 'object')})
-df[objs_df[objs_df['isobj'] == True].index.values] = df[objs_df[objs_df['isobj'] == True].index.values].applymap(
-    lambda x: x.upper() if isinstance(x, str) else x)
-
-# 有phone_book的赋值成1， 空的赋值成0
-df['phone_book'][df['phone_book'].notnull()] = 1
-df['phone_book'][df['phone_book'].isnull()] = 0
-
-# 同盾白骑士审核结果统一
-df['result'] = df['result'].map(lambda x: x.upper() if isinstance(x, str) else 'NODATA')
-df['result'][df['result'].str.match('ACCEPT')] = 'PASS'
-# 有emergency_contact_phone的赋值成1， 空的赋值成0
-df['emergency_contact_phone'][df['emergency_contact_phone'].notnull()] = 1
-df['emergency_contact_phone'][df['emergency_contact_phone'].isnull()] = 0
-
-features_cat = ['check_result', 'result', 'pay_num', 'channel', 'goods_type', 'lease_term', 'type', 'order_type',
-                'source', 'phone_book', 'emergency_contact_phone', 'old_level', 'create_hour', 'sex', ]
-features_number = ['cost', 'daily_rent', 'price', 'age', 'zmf_score', 'xbf_score', ]
-
-for col in df[features_cat + features_number].columns.values:
-    if df[col].dtype == 'O':
-        df[col].fillna(value='NODATA', inplace=True)
-        df.fillna(value=0, inplace=True)
-
-plt.hist(df['check_result'])
-feature_analyse(df, 'result')
-feature_analyse(df, 'pay_num')
-feature_analyse(df, 'channel')
-
-feature = 'zmf_score'
-plt.hist(df[feature])
-feature_analyse(df, feature)
-feature_kdeplot(df, feature)
-
-# 芝麻分分类
-bins = pd.IntervalIndex.from_tuples([(0, 600), (600, 700), (700, 800), (800, 1000)])
-df['zmf_score_band'] = pd.cut(df['zmf_score'], bins, labels=False)
-df[['zmf_score_band', 'check_result']].groupby(['zmf_score_band'], as_index=False).mean().sort_values(by='check_result',
-                                                                                                      ascending=False)
-
-# 小白分分类
-bins = pd.IntervalIndex.from_tuples([(0, 80), (80, 90), (90, 100), (100, 200)])
-df['xbf_score_band'] = pd.cut(df['xbf_score'], bins, labels=False)
-df[['xbf_score_band', 'check_result']].groupby(['xbf_score_band'], as_index=False).mean().sort_values(by='check_result',
-                                                                                                      ascending=False)
-
-# 年龄分类
-bins = pd.IntervalIndex.from_tuples([(0, 18), (18, 24), (24, 30), (30, 40), (40, 100)])
-df['age_band'] = pd.cut(df['age'], bins, labels=False)
-df[['age_band', 'check_result']].groupby(['age_band'], as_index=False).mean().sort_values(by='check_result',
-                                                                                          ascending=False)
-
-# 下单时间分类
-df['create_hour_band'] = pd.cut(df['create_hour'], 5, labels=False)
-df[['create_hour_band', 'check_result']].groupby(['create_hour_band'], as_index=False).mean().sort_values(
-    by='check_result', ascending=False)
-
-features = ['check_result', 'result', 'pay_num', 'channel', 'goods_type', 'type', 'order_type',
-            'source', 'phone_book', 'old_level', 'sex', 'create_hour', 'age_band', 'zmf_score_band',
-            'xbf_score_band', ]
-df = df[features]
-# 类别特征全部转换成数字
-for feature in features:
-    df[feature] = LabelEncoder().fit_transform(df[feature])
-
-# ## 评估结果
-# accuracy： 97.5%  --- 预测正确的个数占样本总数的比率
-# precision： 91.4% --- 预测通过正确的个数占预测通过的比率
-# recall：81.4% --- 预测通过正确的个数占实际通过的比率
-
-# In[42]:
-
-
-# 使用PR曲线： 当正例较少或者关注假正例多假反例。 其他情况用ROC曲线
-plt.figure(figsize=(8, 6))
-plt.xlabel("Recall(FPR)", fontsize=16)
-plt.ylabel("Precision(TPR)", fontsize=16)
-plt.axis([0, 1, 0, 1])
-
-clf = rnd_clf
-y_train_pred = cross_val_predict(clf, x_train, y_train, cv=3)
-y_probas = cross_val_predict(clf, x_train, y_train, cv=3, method="predict_proba", n_jobs=-1)
-y_scores = y_probas[:, 1]  # score = proba of positive class
-precisions, recalls, thresholds = precision_recall_curve(y_train, y_scores)
-plt.plot(recalls, precisions, linewidth=1, label="PR")
-fpr, tpr, thresholds = roc_curve(y_train, y_scores)
-print("{} roc socore: {}".format(clf.__class__.__name__, roc_auc_score(y_train, y_scores)))
-plt.plot(fpr, tpr, linewidth=1, label="ROC")
-
-plt.title("ROC and PR 曲线图")
-plt.legend()
-plt.show()
-
-# ## 总结
-# 1. 机器学习能洞察复杂问题和大量数据，发现内在规律，帮助我们做好数据挖掘方面的工作。
-# 2. 很有很多的改进空间：
-#         a.现有数据特征挖掘，譬如同盾和白骑士中的内容详情，IP地址、收货地址、身份证地址、居住地址中的关联，优惠券，额外服务意外险，每日租金，手机号前三位等。与客户和订单信息相关的数据都可以拿来做特征，提高机器学习预测能力。
-#         b. 增加未被保存到数据库中的特征。
-#         c. 调整机器学习参数，使之更适合做预测分类。
-#         d. 使用更好的模型。XGBoosting、LightGBM和人工神经网络在训练和预测能力上做到更好。
-# 3. 工作计划：
-#         a. 增加特征。需数据库里保存所有订单审核前的所有信息。目前折扣金额和额外服务等数据只有再审核通过后才保存，需更改下。
-#         b. 参考同类型行业，借鉴他人经验，增加相关特征
-#         c. 增加模型预测能力，ROC分数达到0.96以上， 预测准确度达到98.5%
-#         d. 增加客户信用额度字段。如何确定客户额度方案未知。
-#
-# ## 特征处理
-# 1. 把IP地址、收货地址、身份证地址、居住地址转换成经纬度
-# 2. 把创建时间转换成月、日、周几、小时段
-
-# ## 增加特征
-# 1. 下单时的经纬度
-# 2. 下单时手机设备
-# 3.
